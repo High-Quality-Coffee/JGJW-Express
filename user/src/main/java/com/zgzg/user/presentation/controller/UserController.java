8e0b70904c01f4d24b6c3b616c9340e8c8f2b481
High-Quality-Coffee <125748258+High-Quality-Coffee@users.noreply.github.com>
2025-03-15 02:05:19 +0900

    refactor : 필요없는 모듈 파일 삭제 및 gitignore 수정
    refactor :
    - 모듈별 필요없는 파일들 삭제
    - .gitignore 수정을 통해 gradle/wrapper 제외

--- user/src/main/java/com/zgzg/user/presentation/controller/UserController.java
+++ user/src/main/java/com/zgzg/user/presentation/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.zgzg.user.presentation.controller;
 
+import com.zgzg.common.response.Code;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
 
     @GetMapping("/test")
     public String test(){
