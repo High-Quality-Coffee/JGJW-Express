5919085632709aae5908464aba56d4f47a4d5139
Kim jisu <jisu0259@naver.com>
2025-03-17 18:24:03 +0900

    refactor: 불필요 코드 제거
    - scan package
    
    #2

--- company/src/main/java/com/zgzg/company/CompanyApplication.java
+++ company/src/main/java/com/zgzg/company/CompanyApplication.java
@@ -3,12 +3,7 @@ package com.zgzg.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-	scanBasePackages = {
-		"com.zgzg.company",
-		"com.zgzg.common"  // common 모듈의 패키지
-	}
-)
+@SpringBootApplication
 public class CompanyApplication {
 
 	public static void main(String[] args) {
