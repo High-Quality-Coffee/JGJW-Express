0176ed26032ed8bbd16c56d982b0013e6785612d
Kim jisu <jisu0259@naver.com>
2025-03-18 00:53:11 +0900

    feat: Pageable 구현
    #2

---
+++ company/src/main/java/com/zgzg/company/presentation/dto/PageableRequestDTO.java
@@ -0,0 +1,32 @@
+package com.zgzg.company.presentation.dto;
+
+import org.springframework.data.domain.Sort;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageableRequestDTO {
+
+	@Min(0)
+	@Schema(name = "페이지 번호", example = "0")
+	private int page = 0;
+
+	@Min(1)
+	@Schema(name = "페이지 사이즈", example = "10")
+	private int size = 10;
+
+	@Schema(name = "정렬 조건", example = "id")
+	private String sortBy;
+
+	@Schema(name = "정렬 방향", example = "ASC")
+	private Sort.Direction direction = Sort.Direction.ASC;
+
+
+
+}
