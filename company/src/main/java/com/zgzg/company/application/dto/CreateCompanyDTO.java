cf3c07dd38a178c15387ca0144b06e61f4490dca
Kim jisu <jisu0259@naver.com>
2025-03-17 15:16:45 +0900

    feat : swagger 설정
    - 단일 모듈에 swagger 설정

---
+++ company/src/main/java/com/zgzg/company/application/dto/CreateCompanyDTO.java
@@ -0,0 +1,26 @@
+package com.zgzg.company.application.dto;
+
+import com.zgzg.company.domain.Company;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class CreateCompanyDTO {
+
+	@Schema(description = "업체명", example = "test2025")
+	private String name;
+
+	private String type;
+
+	private String address;
+
+	public Company toEntity() {
+		return Company.builder()
+			.name(this.name)
+			.type(this.type)
+			.address(this.address)
+			.build();
+	}
+
+}
