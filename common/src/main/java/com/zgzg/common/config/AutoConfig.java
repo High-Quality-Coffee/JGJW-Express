45c9809f09a4ab74f7fcd7cf92ab54913749ebe5
Ryujy <bamdal124@gmail.com>
2025-03-17 16:23:20 +0900

    feat: common - BaseEntity Auditing 문제 해결 #3

---
+++ common/src/main/java/com/zgzg/common/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.zgzg.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.zgzg.common")
+public class AutoConfig {
+}
