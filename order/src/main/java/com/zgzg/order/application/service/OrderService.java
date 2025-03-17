b08c53e78b0fa3e8ce5b28df817d31f32fb64505
Ryujy <bamdal124@gmail.com>
2025-03-18 01:48:55 +0900

    feat: order - 주문 삭제 api 구현 #3
    # Conflicts:
    #	order/src/main/java/com/zgzg/order/application/service/OrderService.java
    #	order/src/main/java/com/zgzg/order/presentation/controller/OrderController.java

--- order/src/main/java/com/zgzg/order/application/service/OrderService.java
+++ order/src/main/java/com/zgzg/order/application/service/OrderService.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.zgzg.common.exception.BaseException;
+import com.zgzg.common.response.Code;
 import com.zgzg.order.domain.entity.Order;
 import com.zgzg.order.domain.repo.OrderRepository;
 import com.zgzg.order.presentation.dto.req.CreateOrderRequestDto;
@@ -25,7 +27,15 @@ public class OrderService {
 		Order order = requestDto.toEntity(requestDto);
 		log.info("Order creat request: " + order.getReceiverCompanyId());
 		Order savedOrder = orderRepository.save(order);
-		log.info("Order created: " + savedOrder.getReceiverCompanyId());
 		return savedOrder.getOrderId();
+	}
+
+	@Transactional
+	public void deleteOrder(UUID orderId) {
+		Order order = orderRepository.findById(orderId)
+			.orElseThrow(() -> new BaseException(Code.ORDER_NOT_FOUND));
+		// todo. 권한 유효성 검사
+		// todo. 사용자 정보 대입, 임시 데이터 제거
+		order.softDelete("temp1");
 	}
 }
--- order/src/main/java/com/zgzg/order/presentation/controller/OrderController.java
+++ order/src/main/java/com/zgzg/order/presentation/controller/OrderController.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.zgzg.common.response.ApiResponseData;
+import com.zgzg.order.application.dto.res.OrderResponseDto;
 import com.zgzg.order.application.service.OrderService;
 import com.zgzg.order.presentation.dto.req.CreateOrderRequestDto;
 
@@ -37,4 +40,10 @@ public class OrderController {
 		return ResponseEntity.created(uri).build();
 	}
 
+	// todo. 권한 확인 - MASTER, HUB, STORE
+	@PatchMapping("/{orderId}")
+	public ResponseEntity<ApiResponseData<OrderResponseDto>> deleteOrder(@PathVariable UUID orderId) {
+		orderService.deleteOrder(orderId);
+		return ResponseEntity.noContent().build();
+	}
 }
