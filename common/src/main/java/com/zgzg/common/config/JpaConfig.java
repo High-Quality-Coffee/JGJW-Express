a2a3847c44cb71ef616b8b4ca4d68c5d40b00824
Ryujy <bamdal124@gmail.com>
2025-03-19 00:21:55 +0900

    feat: common - querydsl 설정 추가 #18

--- common/src/main/java/com/zgzg/common/config/JpaConfig.java
+++ common/src/main/java/com/zgzg/common/config/JpaConfig.java
@@ -1,10 +1,20 @@
 package com.zgzg.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+		return new JPAQueryFactory(entityManager);
+	}
 
 }
