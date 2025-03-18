66231a46b148fd69118c94c7c2ecf5b3061f6322
Kim jisu <jisu0259@naver.com>
2025-03-19 00:06:28 +0900

    feat: slack message 생성
    - class 생성
    
    #1

--- ai/src/main/java/com/zgzg/ai/application/service/GeminiService.java
+++ ai/src/main/java/com/zgzg/ai/application/service/GeminiService.java
@@ -1,4 +1,9 @@
 package com.zgzg.ai.application.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GeminiService {
+	public void createMessage() {
+	}
 }
--- ai/src/main/java/com/zgzg/ai/presentation/controller/GeminiController.java
+++ ai/src/main/java/com/zgzg/ai/presentation/controller/GeminiController.java
@@ -1,4 +1,26 @@
 package com.zgzg.ai.presentation.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zgzg.ai.application.service.GeminiService;
+import com.zgzg.common.response.ApiResponseData;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController("/api/v1/gemini")
+@RequiredArgsConstructor
 public class GeminiController {
+
+	private final GeminiService geminiService;
+
+	@PostMapping("/")
+	public ResponseEntity<?> createMessage() {
+		geminiService.createMessage();
+		return ResponseEntity.ok().body(ApiResponseData.success("test"));
+	}
+
+
 }
