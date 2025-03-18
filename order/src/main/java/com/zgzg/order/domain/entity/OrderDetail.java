af06e884cb9dcaece300d847b9f741efd9626c81
Ryujy <bamdal124@gmail.com>
2025-03-18 22:14:10 +0900

    feat: order - orderdetail 생성 및 연관관계 매핑 #18

--- order/src/main/java/com/zgzg/order/domain/entity/OrderDetail.java
+++ order/src/main/java/com/zgzg/order/domain/entity/OrderDetail.java
@@ -1,21 +1,18 @@
 package com.zgzg.order.domain.entity;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import com.zgzg.common.utils.BaseEntity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -24,37 +21,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "p_order")
+@Table(name = "p_order_detail")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
-public class Order extends BaseEntity {
+public class OrderDetail extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
-	private UUID orderId;
+	private UUID odId; //주문 상세 고유 id
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	private Order order;
 
 	@Column(nullable = false)
-	private UUID receiverCompanyId;
+	private UUID productId;
 
 	@Column(nullable = false)
-	private UUID supplierCompanyId;
+	private String productName;
 
 	@Column(nullable = false)
-	private String supplierCompanyName;
+	private Integer quantity;
 
 	@Column(nullable = false)
-	private BigDecimal orderTotalPrice;
-
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	private OrderStatus orderStatus;
-
-	@Column(columnDefinition = "TEXT")
-	private String orderRequest;
-
-	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-	@Builder.Default
-	private List<OrderDetail> orderDetails = new ArrayList<>();
+	private BigDecimal productPrice;
 }
