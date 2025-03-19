b3ee26e177fc8cf4885656dc0ac32d56d2875671
Ryujy <bamdal124@gmail.com>
2025-03-19 18:52:32 +0900

    feat: common - JpaConfig에 설정 추가 #22
    - gateway 실행 불가 문제
    - EntityManager 찾지 못하는 오류 발생하여 설정 추가
    
    # Conflicts:
    #	common/src/main/java/com/zgzg/common/config/JpaConfig.java

--- common/src/main/java/com/zgzg/common/config/JpaConfig.java
+++ common/src/main/java/com/zgzg/common/config/JpaConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+
 @Configuration
 @EnableJpaAuditing
 @ConditionalOnClass(name = "jakarta.persistence.EntityManager")
