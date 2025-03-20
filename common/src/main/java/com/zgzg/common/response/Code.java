3a809f229664a106059111917d28b171a40c21e7
Kim jisu <jisu0259@naver.com>
2025-03-20 18:24:56 +0900

    fix: conflict

--- common/src/main/java/com/zgzg/common/response/Code.java
+++ common/src/main/java/com/zgzg/common/response/Code.java
@@ -9,15 +9,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum Code {
-
-    /**
-     * 업체 5000 번대
-     */
-    COMPANY_CREATE(HttpStatus.OK, 5001, "업체 생성이 완료되었습니다."),
-    COMPANY_FIND(HttpStatus.OK,5002,"업체 검색이 완료되었습니다."),
-
-    COMPANY_FIND_ERROR(HttpStatus.BAD_REQUEST, 5101, "아이디와 일치하는 업체가 없습니다."),
-
   /**
    * 성공 0번대
    */
