3d51fffe3617c86198b30b187f20043a8189d11d
Kim jisu <jisu0259@naver.com>
2025-03-20 12:23:13 +0900

    feat: gemini & slack 기능 구현
    #1

--- ai/src/main/java/com/zgzg/ai/AiApplication.java
+++ ai/src/main/java/com/zgzg/ai/AiApplication.java
@@ -2,8 +2,10 @@ package com.zgzg.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.zgzg.common.security", "com.zgzg.ai"})
 public class AiApplication {
 
 	public static void main(String[] args) {
