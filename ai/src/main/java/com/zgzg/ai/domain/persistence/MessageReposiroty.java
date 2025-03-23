ee0108917253bf1bfebbf8a5832eb73f3ebd78de
Kim jisu <jisu0259@naver.com>
2025-03-23 17:17:54 +0900

    feature: 메세지 기능 구현
    - 권한 관리
    - 검색 기능 구현
    
    #1

--- ai/src/main/java/com/zgzg/ai/domain/persistence/MessageReposiroty.java
+++ ai/src/main/java/com/zgzg/ai/domain/persistence/MessageReposiroty.java
@@ -3,7 +3,11 @@ package com.zgzg.ai.domain.persistence;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.zgzg.ai.domain.Message;
 
@@ -11,4 +15,18 @@ public interface MessageReposiroty extends JpaRepository<Message, UUID> {
 
 	Optional<Message> findById(UUID id);
 
+	Optional<Message> findByIdAndDeletedAtIsNull(UUID id);
+
+	@Query("SELECT m FROM Message m " +
+		"WHERE (:keyword IS NULL OR :keyword = '' OR " +
+		"      cast(m.id as string) LIKE concat('%', :keyword, '%') OR " +
+		"      cast(m.receiverId as string) LIKE concat('%', :keyword, '%') OR " +
+		"      lower(m.messageTitle) LIKE lower(concat('%', :keyword, '%')) OR " +
+		"      lower(m.orderNumber) LIKE lower(concat('%', :keyword, '%')) OR " +
+		"      lower(m.originHub) LIKE lower(concat('%', :keyword, '%')) OR " +
+		"      lower(m.finalDestination) LIKE lower(concat('%', :keyword, '%')) OR " +
+		"      cast(m.senderId as string) LIKE lower(concat('%', :keyword, '%')) OR " +
+		"      (:keyword = 'true' AND m.isDelay = true) OR " +
+		"      (:keyword = 'false' AND m.isDelay = false))")
+	Page<Message> searchMessages(@Param("keyword") String keyword, Pageable pageable);
 }
--- ai/src/main/java/com/zgzg/ai/presentation/DTO/MessageResponseDTO.java
+++ ai/src/main/java/com/zgzg/ai/presentation/DTO/MessageResponseDTO.java
@@ -3,6 +3,8 @@ package com.zgzg.ai.presentation.DTO;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.zgzg.ai.domain.Message;
+
 import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,4 +26,22 @@ public class MessageResponseDTO {
 	private String finalDeliveryStartTime;
 	private boolean isDelay;
 	private String delayReason;
+
+	public static MessageResponseDTO toDto(Message message){
+		return MessageResponseDTO.builder()
+			.id(message.getId())
+			.receiverId(message.getReceiverId())
+			.messageTitle(message.getMessageTitle())
+			.orderNumber(message.getOrderNumber())
+			.originHub(message.getOriginHub())
+			.currentLocation(message.getCurrentLocation())
+			.finalDestination(message.getFinalDestination())
+			.estimatedDeliveryTime(message.getEstimatedDeliveryTime())
+			.messageContent(message.getMessageContent())
+			.sentAt(message.getSentAt())
+			.finalDeliveryStartTime(message.getFinalDeliveryStartTime())
+			.isDelay(message.isDelay())
+			.delayReason(message.getDelayReason())
+			.build();
+	}
 }
