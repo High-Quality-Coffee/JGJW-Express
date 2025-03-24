46688538bd8daf1bf2e6489b9759f6a121c41eb5
Ryujy <bamdal124@gmail.com>
2025-03-24 22:09:56 +0900

    feat: 주문/배송 - feignclient 배송 취소 메서드 변경 #48

--- delivery/src/main/java/com/zgzg/delivery/presentation/DeliveryInternalCotroller.java
+++ delivery/src/main/java/com/zgzg/delivery/presentation/DeliveryInternalCotroller.java
@@ -7,13 +7,17 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.zgzg.common.response.ApiResponseData;
+import com.zgzg.delivery.application.dto.res.DeliveryResponseDTO;
 import com.zgzg.delivery.application.service.DeliveryService;
 import com.zgzg.delivery.presentation.dto.req.CreateDeliveryRequestDTO;
 
@@ -40,5 +44,21 @@ public class DeliveryInternalCotroller {
 			.toUri();
 		return ResponseEntity.created(uri)
 			.body(ApiResponseData.success(deliveryID, DELIVERY_CREATE_SUCCESS.getMessage()));
+	}
+
+	@GetMapping("/{deliveryId}")
+	public ResponseEntity<ApiResponseData<DeliveryResponseDTO>> getDelivery(@PathVariable UUID deliveryId) {
+		DeliveryResponseDTO responseDTO = deliveryService.getDelivery(deliveryId);
+		return ResponseEntity.ok()
+			.body(ApiResponseData.of(DELIVERY_READ_SUCCESS.getCode(), DELIVERY_READ_SUCCESS.getMessage(), responseDTO));
+	}
+
+	@PutMapping("/{deliveryId}/cancel")
+	public ResponseEntity<ApiResponseData<DeliveryResponseDTO>> cancelDelivery(
+		@PathVariable UUID deliveryId) {
+		DeliveryResponseDTO responseDTO = deliveryService.cancelDelivery(deliveryId);
+		return ResponseEntity.ok()
+			.body(ApiResponseData.of(DELIVERY_CANCEL_SUCCESS.getCode(), DELIVERY_CANCEL_SUCCESS.getMessage(),
+				responseDTO));
 	}
 }
--- order/src/main/java/com/zgzg/order/infrastructure/client/FeignDeliveryClient.java
+++ order/src/main/java/com/zgzg/order/infrastructure/client/FeignDeliveryClient.java
@@ -4,9 +4,9 @@ import java.util.UUID;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.zgzg.common.response.ApiResponseData;
@@ -19,9 +19,9 @@ public interface FeignDeliveryClient {
 	@PostMapping("/api/v1/deliveries/internal")
 	ApiResponseData<UUID> createDelivery(@RequestBody CreateDeliveryRequestDTO requestDTO);
 
-	@GetMapping("/api/v1/deliveries/{deliveryId}")
-	DeliveryResponseDTO getDelivery(@PathVariable(name = "deliveryId") UUID deliveryId);
-	
-	@PatchMapping("/api/v1/deliveries/{deliveryId}/cancel")
-	ApiResponseData<DeliveryResponseDTO> cancelDelivery(@PathVariable UUID deliveryId);
+	@GetMapping("/api/v1/deliveries/internal/{deliveryId}")
+	DeliveryResponseDTO getDelivery(@PathVariable("deliveryId") UUID deliveryId);
+
+	@PutMapping("/api/v1/deliveries/internal/{deliveryId}/cancel")
+	ApiResponseData<DeliveryResponseDTO> cancelDelivery(@PathVariable("deliveryId") UUID deliveryId);
 }
